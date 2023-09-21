aef79484d5418c9e35b4406a7a7bdce7b27147d6
lucasggmc <lucasggmc@gmail.com>
2023-09-20 23:28:18 -0300

    removed unused code and comments

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,47 +1,47 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config()
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-  export default function sendEmail(req, res){           
+export default function sendEmail(req, res) {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    auth: {
+      user: process.env.USERMAIL,
+      pass: process.env.PASSMAIL,
+    },
+    secure: true,
+  });
 
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        auth: {
-            user: process.env.USERMAIL,
-            pass: process.env.PASSMAIL  
-        }, 
-        secure: true
-    })    
+  const comics = req.body.req;
 
-    const comics = req.body.req
+  let htmlComics = makeHTML(comics);
 
-    let htmlComics = makeHTML(comics);
-
-    transporter.sendMail({  
-        from: `"MARVEL API 👻" <${process.env.USERMAIL}>`, // sender address
-        to: req.body.email, // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: htmlComics, // html body
-      })
-      .then((response) =>{               
-        res.status(200)
-        res.send(response)
-      })
-      .catch((error) =>{            
-        res.status(400)
-        res.send(error)
-      });
+  transporter
+    .sendMail({
+      from: `"MARVEL API 👻" <${process.env.USERMAIL}>`, // sender address
+      to: req.body.email, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: htmlComics, // html body
+    })
+    .then((response) => {
+      res.status(200);
+      res.send(response);
+    })
+    .catch((error) => {
+      res.status(400);
+      res.send(error);
+    });
 }
 
 //monta o body para o envio de e-mail
-function makeHTML(comics){    
-    let body = "";
+function makeHTML(comics) {
+  let body = "";
 
-    comics.map(comic => {
-        const srcIMG = `${comic.thumbnail.path}.${comic.thumbnail.extension}`
-        body += `<h1> ${comic.title} </h1>   <img src="${srcIMG}" width="300" height="500" /> <p> ${comic.description} </p>`; 
-    })
+  comics.map((comic) => {
+    const srcIMG = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+    body += `<h1> ${comic.title} </h1>   <img src="${srcIMG}" width="300" height="500" /> <p> ${comic.description} </p>`;
+  });
 
-    return body;
+  return body;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import Head from 'next/head';
-import { useRef, useState } from 'react';
+import Head from "next/head";
+import { useRef, useState } from "react";
 
 import CardComic from "../components/CardComic";
 import { Header } from "../components/Header";
 
-import axios from 'axios';
-import { api } from '../service/api';
+import axios from "axios";
+import { api } from "../service/api";
 
-import { store } from 'react-notifications-component';
-import styles from './home.module.scss';
-
+import { store } from "react-notifications-component";
+import styles from "./home.module.scss";
 
 type Comic = {
   id: number;
@@ -17,62 +16,61 @@ type Comic = {
   thumbnail: {
     path: string;
     extension: string;
-  },
+  };
   images: {
     path: string;
     extension: string;
-  },
+  };
   description: string;
   urls: [];
-  isSelected: false
-}
+  isSelected: false;
+};
 
 type HomeProps = {
-  comics: Comic[]
-}
+  comics: Comic[];
+};
 
 export default function Home({ comics }: HomeProps) {
-
   const [state, setState] = useState({
-    Comics: comics
+    Comics: comics,
   });
 
-  const [email, setEmail] = useState('')
+  const [email, setEmail] = useState("");
 
-  const inputRef = useRef<HTMLInputElement>(null);        
-  const [filter, setFilter] = useState('');  
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [filter, setFilter] = useState("");
 
-  function findComics(){
-    let textSearched = inputRef.current.value;    
+  function findComics() {
+    let textSearched = inputRef.current.value;
 
-    setFilter(textSearched)
-  }  
+    setFilter(textSearched);
+  }
 
   //chama o método que faz o envio de e-mail na API
-  function send(comicsToSend){    
-    axios.post(`/api/sendEmail`, {req: comicsToSend, email: email}).then(response => {       
-      showNotification(200);    
-    }).catch(error => {             
-      showNotification(400); 
-    });    
-
+  function send(comicsToSend) {
+    axios
+      .post(`/api/sendEmail`, { req: comicsToSend, email: email })
+      .then((response) => {
+        showNotification(200);
+      })
+      .catch((error) => {
+        showNotification(400);
+      });
   }
 
-  function verifyComicsToSend(){  
-    if(email == '')
-      return;
+  function verifyComicsToSend() {
+    if (email == "") return;
 
-    const comicsSelected = state.Comics.filter(comic => comic.isSelected);
-    if(comicsSelected.length != 0)
-      send(comicsSelected);
+    const comicsSelected = state.Comics.filter((comic) => comic.isSelected);
+    if (comicsSelected.length != 0) send(comicsSelected);
   }
 
-  function showNotification(status: number){
+  function showNotification(status: number) {
     let title = "E-mail sent";
     let message = "Check your inbox";
     let type = "success";
 
-    if(status === 400){
+    if (status === 400) {
       title = "Email not sent";
       message = "An error occurred sending email";
       type = "danger";
@@ -88,8 +86,8 @@ export default function Home({ comics }: HomeProps) {
       animationOut: ["animate__animated", "animate__fadeOut"],
       dismiss: {
         duration: 5000,
-        onScreen: true
-      }
+        onScreen: true,
+      },
     });
   }
 
@@ -97,64 +95,59 @@ export default function Home({ comics }: HomeProps) {
     <div>
       <Head>
         <title>MARVEL | Comics</title>
-      </Head>      
+      </Head>
       <Header />
 
       <div className={styles.searchBox}>
-        <input type="text" 
-        placeholder="Find comic" 
-        //value={filter}
-        //onChange={event => setFilter(event.target.value)}
-        ref={inputRef}       
-        />
+        <input type="text" placeholder="Find comic" ref={inputRef} />
         <button type="button" onClick={findComics}>
           <img src="/icons/arrow-button.svg" />
         </button>
       </div>
 
-      <div className={styles.cardsComic}>        
-        {
-          state.Comics.filter(comic => comic.title.toLowerCase().includes(filter) || filter === '').map((comic, index) => {                                         
-            return (                    
-              <div key={comic.id}>              
-              <CardComic key={comic.id} comic={comic}/> 
-              </div>
-            );
-          })
-        }        
-      </div>
-      
-      <div className={styles.info}>
-        <p>You can select and send some comics by email!</p>        
+      <div className={styles.cardsComic}>
+        {state.Comics.filter(
+          (comic) => comic.title.toLowerCase().includes(filter) || filter === ""
+        ).map((comic, index) => {
+          return (
+            <div key={comic.id}>
+              <CardComic key={comic.id} comic={comic} />
+            </div>
+          );
+        })}
       </div>
 
-      <div className={styles.footerEmail}>       
-        <input type="text" 
-        placeholder="Your best email" 
-        onChange={(e) => {setEmail(e.target.value)}}                  
+      <div className={styles.info}>
+        <p>You can select and send some comics by email!</p>
+      </div>
+
+      <div className={styles.footerEmail}>
+        <input
+          type="text"
+          placeholder="Your best email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
 
         <button type="button" onClick={verifyComicsToSend}>
           Send!
         </button>
       </div>
-     
     </div>
-  )
+  );
 }
 
 //maneira SSR
-export async function getServerSideProps() {  
-
-  const { data } = await api.get('comics', {
+export async function getServerSideProps() {
+  const { data } = await api.get("comics", {
     params: {
-      format: 'comic',
-      limit: 3          
-    }
-  })  
+      format: "comic",
+      limit: 3,
+    },
+  });
 
-  const comics = data.data.results.map(comic => {           
-
+  const comics = data.data.results.map((comic) => {
     return {
       id: comic.id,
       title: comic.title,
@@ -162,14 +155,13 @@ export async function getServerSideProps() {
       images: comic.images,
       urls: comic.urls,
       description: comic.description,
-      isSelected: false
+      isSelected: false,
     };
+  });
 
-  })
-    
-    return {
+  return {
     props: {
-      comics: comics
-    }
+      comics: comics,
+    },
   };
 }
